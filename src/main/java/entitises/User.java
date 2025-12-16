eadff61b9420101c12c151d891281ce25f700d00
Assia TAHTAOUI <129065960+assiatahtaoui@users.noreply.github.com>
2025-12-16 01:50:00 +0100

    Update User.java

--- src/main/java/entitises/User.java
+++ src/main/java/entitises/User.java
@@ -1,4 +1,38 @@
 package entitises;
 
 public class User {
+  private int userId;
+    private int balance;
+
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "balance=" + balance +
+                ", userId=" + userId +
+                '}';
+    }
+
+    public User(int userId, int balance) {
+        this.userId = userId;
+        this.balance = balance;
+
+
+    }
 }
